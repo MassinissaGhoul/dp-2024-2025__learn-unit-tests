5825d918508f4f69aa070596b5f9523acf31aaa0
TenixOue <79940202+TenixOue@users.noreply.github.com>
2025-03-11 11:10:02 +0100

    class

--- src/test/java/fr/anthonyquere/teashoptest/TeaShopTest.java
+++ src/test/java/fr/anthonyquere/teashoptest/TeaShopTest.java
@@ -1,4 +1,10 @@
 package fr.anthonyquere.teashoptest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TeaShopTest {
+    @Test
+    void
 }
